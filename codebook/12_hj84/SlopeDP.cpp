4890ee2a3a7d81b3b03fc0b79ec2e9e9ded9fd1b
Brian Li <b09902054@csie.ntu.edu.tw>
2023-09-08 14:14:58 +0800

    add SlopeDP

---
+++ codebook/12_hj84/SlopeDP.cpp
@@ -0,0 +1,29 @@
+struct L {
+  mutable lld a, b, p;
+  bool operator<(const L &r) const {
+    return a < r.a; /* here */ }
+  bool operator<(lld x) const { return p < x; }
+};
+lld Div(lld a, lld b) {
+  return a / b - ((a ^ b) < 0 && a % b); };
+struct DynamicHull : multiset<L, less<>> {
+  static const lld kInf = 1e18;
+  bool Isect(iterator x, iterator y) {
+    if (y == end()) { x->p = kInf; return false; }
+    if (x->a == y->a)
+      x->p = x->b > y->b ? kInf : -kInf; /* here */
+    else x->p = Div(y->b - x->b, x->a - y->a);
+    return x->p >= y->p;
+  }
+  void Insert(lld a, lld b) {
+    auto z = insert({a, b, 0}), y = z++, x = y;
+    while (Isect(y, z)) z = erase(z);
+    if (x!=begin()&&Isect(--x,y)) Isect(x, y=erase(y));
+    while ((y = x) != begin() && (--x)->p >= y->p)
+      Isect(x, erase(y));
+  }
+  lld Query(lld x) {   // default chmax
+    auto l = *lower_bound(x);  // to chmin:
+    return l.a * x + l.b;      // modify the 2 "<>"
+  }
+};
